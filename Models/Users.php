80a9af165b7eb72a9c1a9882c2020834dc6fa386
Milad <mniroee@gmail.com>
2025-04-08 11:06:50 +0330

    add missing return to users model

--- Models/Users.php
+++ Models/Users.php
@@ -9,6 +9,6 @@ class Users extends Model
     public function getUsers()
     {
         $Query = "SELECT * FROM users WHERE `is_active` = 1";
-        $this->SelectRow($Query);
+        return $this->SelectRow($Query);
     }
 }
